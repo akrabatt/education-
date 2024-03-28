3342c4fda0cbdaad6dc40540d9f34c36057bb8f5
akrabatt <Starwars-1999@yandex.ru>
2024-03-28 22:39:05 +0200

    ind_array chngs_1

--- ind_array/ind_array.cpp
+++ ind_array/ind_array.cpp
@@ -2,13 +2,13 @@
 
 int main() {
 
-    int ar[10] {};
+    int ar[10] {};  //создали массив
 
-    int *ind;
-    ind = ar;
+    int *ind;   //создали указатель
+    ind = ar;   //указываем на массив
 
-    for(int i = 0; i < sizeof(ar) / sizeof(*ar); i++) {
-        ind += 1;
+    for(int i = 0; i < sizeof(ar) / sizeof(*ar); i++) {     //запускаем цикл в котором делаем подсчет по колличеству элементов в массиве
+        ind += 1;   
         *ind = i+1;
         std::cout << *ind << std::endl;
     }
