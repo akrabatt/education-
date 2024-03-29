a99b94f9fddf119fb84dc9c916059ca16ddfef03
Dmitrii_rz <starwars-1999@yandex.ru>
2024-03-29 09:31:07 +0200

    ind_struct 2th commit basic operation

--- ind_struct/ind_struct.cpp
+++ ind_struct/ind_struct.cpp
@@ -15,6 +15,10 @@ int main() {
     struct tag_vector *prt_v;   //создадим указатель
     prt_v = &v;      //присвоем указателю адрес структуры
 
-    
+    prt_v -> x = 5.0;   //присваеваем элементу структуры новое значение
+    prt_v -> y = 3.0;
+
+    std::cout << prt_v -> x << std::endl;
+    std::cout << prt_v -> y << std::endl;
 
 }
