bcd606a78a68d5cb4e1bf007a7908e5a82bb1336
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-02 14:20:50 +0200

    end tast_2

--- exercises/task_2_str_mass/task_2_str_mass.cpp
+++ exercises/task_2_str_mass/task_2_str_mass.cpp
@@ -10,10 +10,17 @@ int main() {
 
     tag_employes *ptr = &employes[0];   //создадим указатель на массив структур
 
-    std::cout << sizeof(employes) << std::endl; //размер всего массива
-    std::cout << sizeof(employes[0]) << std::endl; //размер одного элемента массива
-    std::cout << employes << std::endl; //выведет адресс первого элемента массива  
-    std::cout << ptr << std::endl;  //указатель выведет адрес первого элемента массива
+
+    /* размер массива */
+    std::cout << "array size: " << sizeof(employes) << std::endl; //размер всего массива
+    std::cout << "element of array size: " << sizeof(employes[0]) << std::endl << std::endl; //размер одного элемента массива
+
+    /* вывести элемент массива */
+    std::cout << "employes: " << employes << std::endl; //выведет адресс первого элемента массива  
+    std::cout << "employes[0] addr: " << &employes[1] << std::endl;  //выведет тоже адресс первого элемента массива
+    std::cout << "pointer: " << ptr << std::endl;  //указатель выведет адрес первого элемента массива
+    std::cout << "mass adr size: " << sizeof(employes)/sizeof(employes[0]) << std::endl;    //выведем количество элементов массива
+    std::cout << "mass adr : " << employes + sizeof(employes)/sizeof(employes[0]) << std::endl << std::endl;    //выведем размер последнего элемента для цикла
 
     /* далее создадим цикл ввода данных в массив структур */
     for(ptr = &employes[0]; ptr < employes + sizeof(employes)/sizeof(employes[0]); ptr++) {
@@ -21,5 +28,11 @@ int main() {
         std::cin >> ptr->age;
         std::cout << std::endl;
     }
-
+    std::cout << std::endl;
+    /* далее выведем все что мы вбили */
+    for(ptr = &employes[0]; ptr < employes + sizeof(employes)/sizeof(employes[0]); ptr++) {
+        std::cout << "output age: " << ptr->age << std::endl;
+    }
+    int a = 0;
+    std::cin >>a;
 }
