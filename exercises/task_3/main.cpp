7fc2a1c0df9928d594fe2a9ee74319921a515e0b
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-04 15:28:25 +0200

    task_3 point 2

--- exercises/task_3/main.cpp
+++ exercises/task_3/main.cpp
@@ -12,6 +12,8 @@ int main() {
     // ptr->GROUP = 1122;
     // ptr->SES[0] = 5;
 
+    std::cout << sizeof(ptr->SES)/sizeof(ptr->SES[0]) << std::endl; //тестовая строчка, выводит количество элементов в массиве где надо будет проставить оценки 
+
     for(ptr = &student_[0]; ptr < student_ + sizeof(student_)/sizeof(student_[0]); ptr++) {     //начинаем цикл заполнения информации
 
         /* заполняем имя студента */
@@ -24,6 +26,13 @@ int main() {
         std::cin >> ptr->GROUP;
         std::cout << std::endl;
 
+        /* теперь нам необходимо запустить вложенный цикл для ввода успеваемости студента */
+        for (int i = 0; i != sizeof(ptr->SES)/sizeof(ptr->SES[0]); i++) {
+            std::cout << "введите оценку за " << i << "-й предмет: ";
+            std::cin >> ptr->SES[i];
+            std::cout << std::endl;
+        }
+        
         
     }
 }
