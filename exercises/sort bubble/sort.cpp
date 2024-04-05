23b86f3ea9bbd9b663e7c205fdbf04997cf03ead
akrabatt <Starwars-1999@yandex.ru>
2024-04-06 01:40:06 +0200

    end day

--- exercises/sort bubble/sort.cpp
+++ exercises/sort bubble/sort.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
 int main() {
+    int arr[10];
+    int *ptr = arr;
+
+
+    std::cout << "arr size: " << sizeof(arr)/sizeof(arr[0]);
+    std::cout << std::endl;
+
+    for (int i = 0; i != sizeof(arr)/sizeof(arr[0]); i++)
+    {
+        // std::cin >> arr[i];
+        std::cin >> ptr;
+        std::cout << std::endl;
+        if(i == sizeof(arr)/sizeof(arr[0])) {
+
+        }
+    }
     
 }
