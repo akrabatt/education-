5c2f6cb7d66388303e2db4ae466063fc5cacc914
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-11 10:48:11 +0200

    point

--- exercises/task_3_students/main.cpp
+++ exercises/task_3_students/main.cpp
@@ -5,40 +5,40 @@ STUDENT student_[3];   //создадим массив структур
 
 int main() {
 
-    STUDENT *ptr = student_;    //создадим указатель на массив структур
+    STUDENT *ptr_1 = student_;    //создадим указатель на массив структур
 
     /* заполним некоторые данные */
-    // ptr->NAME = "dima";
-    // ptr->GROUP = 1122;
-    // ptr->SES[0] = 5;
-    // ptr->mid = 10;
-    // std::cout << ptr->mid << std::endl;
+    // ptr_1->NAME = "dima";
+    // ptr_1->GROUP = 1122;
+    // ptr_1->SES[0] = 5;
+    // ptr_1->mid = 10;
+    // std::cout << ptr_1->mid << std::endl;
 
 
-    std::cout << sizeof(ptr->SES)/sizeof(ptr->SES[0]) << std::endl; //тестовая строчка, выводит количество элементов в массиве где надо будет проставить оценки 
+    std::cout << sizeof(ptr_1->SES)/sizeof(ptr_1->SES[0]) << std::endl; //тестовая строчка, выводит количество элементов в массиве где надо будет проставить оценки 
 
     /* НАЧИНАЕМ ЗАПОЛНЕНИЕ ИНФОРМАЦИИ О СТУДЕНТАХ */
-    for(ptr = &student_[0]; ptr < student_ + sizeof(student_)/sizeof(student_[0]); ptr++) {     //начинаем цикл заполнения информации
+    for(ptr_1 = &student_[0]; ptr_1 < student_ + sizeof(student_)/sizeof(student_[0]); ptr_1++) {     //начинаем цикл заполнения информации
 
         /* ИМЯ СТУДЕНТА */
         std::cout << "input student's name: ";
-        std::cin >> ptr->NAME;
+        std::cin >> ptr_1->NAME;
         std::cout << std::endl;
 
         /* НОМЕР ГРУППЫ СТУДЕНТА */
         std::cout << "input student's group number: ";
-        std::cin >> ptr->GROUP;
+        std::cin >> ptr_1->GROUP;
         std::cout << std::endl;
 
         /* теперь нам необходимо запустить вложенный цикл для ввода успеваемости студента */
-        for (int i = 0; i != sizeof(ptr->SES)/sizeof(ptr->SES[0]); i++) {
+        for (int i = 0; i != sizeof(ptr_1->SES)/sizeof(ptr_1->SES[0]); i++) {
             std::cout << "input " << i+1 << "-th subject's mark: ";
-            std::cin >> ptr->SES[i];
+            std::cin >> ptr_1->SES[i];
             std::cout << std::endl;
-            ptr->mid += ptr->SES[i];
+            ptr_1->mid += ptr_1->SES[i];
         }
-        ptr->mid /= sizeof(ptr->SES)/sizeof(ptr->SES[0]);   //здесь вычисляем срдний бал студента 
-        std::cout << "mid mark: " << ptr->mid << std::endl << std::endl;
+        ptr_1->mid /= sizeof(ptr_1->SES)/sizeof(ptr_1->SES[0]);   //здесь вычисляем срдний бал студента 
+        std::cout << "mid mark: " << ptr_1->mid << std::endl << std::endl;
     }
 
     /* ТЕПЕРЬ ВЫПОЛНИМ ПУЗЫРЬКОВУЮ СОРТИРОВКУ С ПОМОЩЬЮ SWAP*/
@@ -63,10 +63,10 @@ int main() {
     //     }
     // }
 
-    ptr = &student_[0];
+    ptr_1 = &student_[0];
     for (int i = 0; i < sizeof(student_)/sizeof(student_[0]); i++) {
-        std::cout << ptr->NAME << std::endl;
-        ptr++;
+        std::cout << ptr_1->NAME << std::endl;
+        ptr_1++;
     }
 
 }
