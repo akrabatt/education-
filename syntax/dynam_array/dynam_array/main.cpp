8a6ef477db7c23d1a05f27e25d24074aafe3201c
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-15 10:36:54 +0200

    dyn arr

--- syntax/dynam_array/dynam_array/main.cpp
+++ syntax/dynam_array/dynam_array/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    
-    std::cout << "hello world" << std::endl;
+
+    int size;  //переменная для работы с размером массива
+
+
+    std::cout << "input array size: " << std::endl;
+    std::cin >> size;
+    std::cout << std::endl;
+
+    int *ptr = new int[size];   //создадим указатель на массив 
+
+    /* теперь давайте заполним этот массив */
+    for (int i = 0; i < size; i++) {
+        ptr[i] = i + 1;     //заполним массив
+        std::cout << ptr[i] << "\t" << ptr + i << std::endl;    //выведем каждый эллемент массива и его адреса
+    }
+
+    std::cout << std::endl  << ptr[2] << "\t" << &ptr[2] << std::endl;  //выведем какой нибудь элемент из этого массива
+
+
+    delete [] ptr;  //обязательно удалим массив очистим память
 }
