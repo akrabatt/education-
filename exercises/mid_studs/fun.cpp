6df6e8144759574f63858e7b2fa5831baedcdc1b
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-17 11:41:52 +0200

    fun v1 done

--- exercises/mid_studs/fun.cpp
+++ exercises/mid_studs/fun.cpp
@@ -2,18 +2,20 @@
 #include "str_stud.h"
 
 /* создаем функцию с указателем на структуру и заполняем в ней инормацию про студентов */
-void add_stud(struct STUD *std) // будем передавать в функцию указатель на структуру
+void add_stud(STUD *std) // будем передавать в функцию указатель на структуру
 {
-    for (int i = 0; i < sizeof(std) / sizeof(std[0]) - 1; i++) // тут мы запускаем цикл по заполнению структуры (обязательно -1)
-    {
-        std::cout << "input students name" << std::endl;
+    // for (int i = 0; i < sizeof(std) / sizeof(std[0]) - 1; i++) // тут мы запускаем цикл по заполнению структуры (обязательно -1)
+    // {
+        std::cout << "input students name: ";
         std::cin >> std->NAME; // заполняем имя студента
 
         /* далее запускаем цикл по заполнению массива с оценками студента в структуре */
-        for (size_t i = 0; i < count; i++)
+        for (int j = 0; j < sizeof(std->MARK) / sizeof(std->MARK[0]); j++) // запускаем цикл заполнения (обязательно -1)
         {
-            /* code */
+            std::cout << "input " << j + 1 << "-th mark: ";
+            std::cin >> std->MARK[j]; // тут вставляем оценку
+            std::cout << std::endl;
         }
-        
-    }
+
+    // }
 }
--- exercises/mid_studs/main.cpp
+++ exercises/mid_studs/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "str_stud.h"
 
-STUD student_g1[3];
-STUD student_g2[3];
+extern void add_stud(struct STUD *std); // объявим внешнюю функцию по заполнению данных о студентах
 
+STUD student_g1[3]; // создадим переменную структуры
+STUD student_g2[3];
 
 int main()
 {
+    for (int i = 0; i < 3; i++)
+    {
+        add_stud(&student_g1[i]);
+    }
+    // add_stud(&student_g1[1]);
 }
