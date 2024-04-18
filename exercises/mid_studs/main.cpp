6164d40ae31464c60f3742c3186ac7a2c0f0ab35
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-18 13:35:25 +0200

    done fun mid makr

--- exercises/mid_studs/main.cpp
+++ exercises/mid_studs/main.cpp
@@ -2,12 +2,18 @@
 #include "str_stud.h"
 
 extern void add_stud(struct STUD *std); // объявим внешнюю функцию по заполнению данных о студентах
+extern void mid_range(struct STUD *std);
 
 STUD student_g1[3]; // создадим переменную структуры
 STUD student_g2[3];
 
 int main()
 {
+    /* вызываем функции на заполнение массивов структу */
     add_stud(student_g1);
-    add_stud(student_g2);
+    // add_stud(student_g2);
+
+    /* вызываем функции на вычисление среднего балла по группе */
+    mid_range(student_g1);
+    mid_range(student_g2);
 }
