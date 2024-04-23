ae31a6ed547a31e57034e92bbb1795c2b4b14ae0
akrabatt <starwars-1999@yandex.ru>
2024-04-23 23:05:09 +0200

    new

--- syntax/vectors/struct_vectors/main.cpp
+++ syntax/vectors/struct_vectors/main.cpp
@@ -7,15 +7,15 @@ VECT vect;
 int main()
 {
     /* создадим вектор структур, сама структура в другом файле */
-    std::vector<VECT> vect;                    // создали вектор
-    vect.resize(3);                            // дадим размер вектору
-    std::cout << vect.size() << std::endl;     // узнаем размер
-    std::cout << vect.capacity() << std::endl; // узнаем емкость
-    vect.push_back({1, 2, 10});                // дадим первой структуре значения
-    vect.resize(4);                            // изменим размер вектора
-    std::cout << vect.size() << std::endl;
-    std::cout << vect.capacity() << std::endl;
-    vect.resize(20); // дадим другое значение вектору
+    std::vector<VECT> vect;                                    // создали вектор
+    vect.resize(3);                                            // дадим размер вектору
+    std::cout << "size: " << vect.size() << std::endl;         // узнаем размер
+    std::cout << "capacity: " << vect.capacity() << std::endl; // узнаем емкость
+    vect.push_back({1, 2, 10});                                // дадим первой структуре значения
+    vect.resize(4);                                            // изменим размер вектора
+    std::cout << "size: " << vect.size() << std::endl;         // узнаем размер
+    std::cout << "capacity: " << vect.capacity() << std::endl; // узнаем емкость
+    vect.resize(20);                                           // дадим другое значение вектору
     /* поприсваиваем значения таким способом */
     vect[0].a = 10;
     vect[1].a = 20;
@@ -28,19 +28,13 @@ int main()
     vect[8].a = 20;
 
     /* выведем значения таким способом */
-    std::cout << vect[0].a << std::endl;
-    std::cout << vect[1].a << std::endl;
-    std::cout << vect[2].a << std::endl;
-    std::cout << vect[3].a << std::endl;
-    std::cout << vect[4].a << std::endl;
-    std::cout << vect[5].a << std::endl;
-    std::cout << vect[6].a << std::endl;
-    std::cout << vect[7].a << std::endl;
-    std::cout << vect[8].a << std::endl;
+    // std::cout << vect[0].a << std::endl;
+    // std::cout << vect[1].a << std::endl;
+    // std::cout << vect[2].a << std::endl;
 
     /* узнаем емкость и размер вектора */
-    std::cout << vect.size() << std::endl;
-    std::cout << vect.capacity() << std::endl;
+    std::cout << "size: " << vect.size() << std::endl;         // узнаем размер
+    std::cout << "capacity: " << vect.capacity() << std::endl; // узнаем емкость
 
     /* назначим указатель вектору */
     VECT *ptr = &vect[0]; // соблюдаем именно такой синтаксис
