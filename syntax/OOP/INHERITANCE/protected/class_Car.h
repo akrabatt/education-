4ddc3d492d9bae4648775c4fd6fccefa563b8151
akrabatt <starwars-1999@yandex.ru>
2024-06-05 16:54:46 +0200

    end day

---
+++ syntax/OOP/INHERITANCE/protected/class_Car.h
@@ -0,0 +1,12 @@
+#ifndef CLASS_CAR_H
+#define CLASS_CAR_H
+#include <string>
+
+class Car
+{
+protected:
+    // имя 
+    std::string name;
+};
+
+#endif // CLASS_CAR_H
---
+++ syntax/OOP/INHERITANCE/protected/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+// классы
+#include "./class_Car.h"
+
+
+int main()
+{
+
+}
