188d2bff0a456ae57465e0cf59372320ec681719
akrabatt <starwars-1999@yandex.ru>
2024-06-05 23:12:02 +0200

    again end day

--- syntax/OOP/INHERITANCE/protected/class_Car.h
+++ syntax/OOP/INHERITANCE/protected/class_Car.h
@@ -5,8 +5,21 @@
 class Car
 {
 protected:
-    // имя 
-    std::string name;
+    // имя
+    std::string brand;
+
+    // виртуальный деструктор
+    virtual ~Car() { std::cout << "Car has been deleted\n"; }
+
+public:
+    // установить марку машины
+    void SetBrand(const std::string brand) { this->brand = brand; }
+
+    // получить название марки машины
+    std::string GetBrand() { return brand; }
+
+    // конструктор
+    Car(std::string brand) : brand(brand) {}
 };
 
 #endif // CLASS_CAR_H
