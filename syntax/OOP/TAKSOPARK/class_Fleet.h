b6cfc7cf1ea10f06d4cd449b6408b0f21f285f39
akrabatt <starwars-1999@yandex.ru>
2024-06-11 23:55:20 +0200

    new metod

--- syntax/OOP/TAKSOPARK/class_Fleet.h
+++ syntax/OOP/TAKSOPARK/class_Fleet.h
@@ -7,14 +7,27 @@
 class Fleet
 {
 private:
-    // контейнер с водителями
+    // контейнер со свободными водителями
     std::list<std::shared_ptr<Driver>> drivers_list;
 
-    // контейнер с машинами
+    // контейнер со свободными машинами
     std::list<std::shared_ptr<Car>> cars_list;
 
     // мап связка водитель - машина
     std::map<std::shared_ptr<Driver>, std::shared_ptr<Car>> drivers_cars_map;
+
+public:
+    // деструктор
+    ~Fleet(){};
+
+    // конструктор по умолчанию
+    Fleet() { std::cout << "the Fleet " << this << " has been created!\n"; }
+
+    // добавить свободного водителя
+    void SetAddFreeDriver(std::shared_ptr<Driver> new_driver)
+    {
+        this->drivers_list.push_back(new_driver);
+    }
 };
 
 #endif
