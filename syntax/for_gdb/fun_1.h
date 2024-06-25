8f78dc0348b6047e53cb5b3ff030e3a828b0b186
akrabatt <starwars-1999@yandex.ru>
2024-06-25 23:16:08 +0200

    save point

---
+++ syntax/for_gdb/fun_1.h
@@ -0,0 +1,6 @@
+#ifndef FUN_1
+#define FUN_1
+
+int fun_1(int a, int b);
+
+#endif //FUN_1
---
+++ syntax/for_gdb/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "1 hello !\n";
+    std::cout << "2 hello !\n";
+    std::cout << "3 hello !\n";
+    std::cout << "4 hello !\n";
+    std::cout << "5 hello !\n";
+    std::cout << "6 hello !\n";
+    std::cout << "7 hello !\n";
+    std::cout << "8 hello !\n";
+    std::cout << "9 hello !\n";
+    std::cout << "10 hello !\n";
+    std::cout << "12 hello !\n";
+    std::cout << "13 hello !\n";
+    std::cout << "14 hello !\n";
+    std::cout << "15 hello !\n";
+    std::cout << "16 hello !\n";
+    std::cout << "17 hello !\n";
+    std::cout << "18 hello !\n";
+    std::cout << "19 hello !\n";
+    std::cout << "20 hello !\n";
+    std::cout << "21 hello !\n";
+    std::cout << "22 hello !\n";
+    std::cout << "23 hello !\n";
+    std::cout << "24 hello !\n";
+    std::cout << "25 hello !\n";
+    std::cout << "26 hello !\n";
+    std::cout << "27 hello !\n";
+    std::cout << "28 hello !\n";
+    std::cout << "29 hello !\n";
+    std::cout << "30 hello !\n";
+}
